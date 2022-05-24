25c927ee00dcc87708969fd8804f7d02131e310b
itstheanurag <gauravanurag36@gmail.com>
2022-05-24 12:22:35 +0530

    First commit

---
+++ src/model/userModel.js
@@ -0,0 +1,76 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    fname : {
+        type : String,
+        required : true,
+        trim : true
+    },
+    lname :{
+        type : String,
+        required : true,
+        trim : true
+    },
+    email : {
+        type : String,
+        required : true,
+        trim : true,
+        unique: true
+    },
+    profileImage : {
+        type : String,
+    },
+    password : {
+        type : String,
+        required : true,
+        trim : true,
+        minlength : 8,
+        maxlength : 15
+    },
+    phone  : {
+        type : String,
+        required : true,
+        unique : true,
+        trim : true
+        //should be indian
+    },
+    address : {
+        shipping : {
+            city : {
+                type: String,
+                required : true,
+                trim : true
+            },
+            street : {
+                type : String,
+                required : true,
+                trim : true
+            },
+            pincode : {
+                type : Number,
+                required : true,
+                //should be valid pincode
+            }
+        },
+        billing : {
+            city : {
+                type: String,
+                required : true,
+                trim : true
+            },
+            street : {
+                type : String,
+                required : true,
+                trim : true
+            },
+            pincode : {
+                type : Number,
+                required : true,
+                //should be valid pincode
+            }
+        }
+    }
+    
+}, {timestamps : true})
+
+module.exports = new mongoose.model('user', userSchema)
---
+++ src/validations/userValidations.js
@@ -0,0 +1,37 @@
+const {check, validationResult} = require("express-validator")
+
+exports.userValidation = [check('fname')
+.trim()
+.not()
+.isEmpty().withMessage('first name is Missing')
+.matches(/^[a-zA-Z ]+$/g)
+.withMessage('this is not a first name'),
+
+check('lname')
+.trim()
+.not()
+.isEmpty().withMessage('last name is Missing')
+.matches(/^[a-zA-Z ]+$/g)
+.withMessage('this is not a first name'),
+
+check('email')
+.not()
+.isEmpty().withMessage('Email is a required field')
+.normalizeEmail()
+.isEmail()
+.withMessage('Email is Invalid'),
+
+check('phone')
+.trim()
+.not()
+.isEmpty().withMessage('Phone number is missing')
+.matches('^[6789][0-9]{9}$')
+.withMessage('Not a valid phone number'),
+
+check('password')
+.trim()
+.not()
+.isEmpty().withMessage('password is Missing')
+.isLength({ min: 8, max: 15})
+.withMessage(' Password must be within 8 to 15 characters long'),
+]
