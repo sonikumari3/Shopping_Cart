b1fd102649b3b7cbf2ad5c5dee8da50378a17193
itstheanurag <gauravanurag36@gmail.com>
2022-05-24 14:12:48 +0530

    User Login Api

--- src/controller/userControllers.js
+++ src/controller/userControllers.js
@@ -1,6 +1,7 @@
 const user = require('../model/userModel')
 const aws = require('aws-sdk')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
 
 const isValidRequestBody = function (value) {
@@ -16,8 +17,6 @@ const isValidRequestBody = function (value) {
   }
   
  
-  
-  
   aws.config.update({
     accessKeyId: "AKIAY3L35MCRUJ6WPO6J",
     secretAccessKey: "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",
@@ -86,7 +85,7 @@ const isValidRequestBody = function (value) {
         if (!isValid(email)) {
             return res.status(400).send({ status: false, message: "plzz enter email" })
         }
-        const emailPattern = /^\w+([\.-]?\w+)@\w+([\.-]?\w+)(\.\w{2,3})/       //email regex validation for validate the type of email.
+        const emailPattern = /^\w+([\.-]?\w+)@\w+([\.-]?\w+)(\.\w{2,3})/g       //email regex validation for validate the type of email.
 
         if (!email.match(emailPattern)) {
             return res.status(400).send({ status: false, message: "This is not a valid email" })
@@ -102,7 +101,7 @@ const isValidRequestBody = function (value) {
         if (!isValid(password)) {
             return res.status(400).send({ status: false, message: "plzz enter password" })
         }
-        if (password.length < 8 || password.length > 15) {
+        if (password.trim().length < 8 || password.trim().length > 15) {
             return res.status(400).send({ status: false, message: "plzz enter valid password" })
         }
 
@@ -116,7 +115,7 @@ const isValidRequestBody = function (value) {
         }
 
         //this regex will to set the phone no. length to 10 numeric digits only.
-        if (!/^(\+91)?0?[6-9]\d{9}$/.test(phone)) {
+        if (!/^(\+91)?0?[6-9]\d{9}$/g.test(phone)) {
             return res.status(400).send({ status: false, message: "Please enter valid 10 digit mobile number." })
         }
 
@@ -153,7 +152,7 @@ const isValidRequestBody = function (value) {
                 if (!isValid(shipping.city)) {
                     return res.status(400).send({ status: false, message: "shipping city is required" })
                 }
-                if (!/^[a-zA-Z]+$/.test(shipping.city)) {
+                if (!/^[a-zA-Z]+$/g.test(shipping.city)) {
                     return res.status(400).send({ status: false, message: "city field have to fill by alpha characters" });
                 }
     
@@ -163,7 +162,7 @@ const isValidRequestBody = function (value) {
                 }
 
                  //applicable only for numeric values and extend to be 6 characters only--
-                if (!/^\d{6}$/.test(shipping.pincode)) {
+                if (!/^\d{6}$/g.test(shipping.pincode)) {
                     return res.status(400).send({ status: false, message: "plz enter valid pincode" });
                 }
 
@@ -184,7 +183,7 @@ const isValidRequestBody = function (value) {
                 if (!isValid(billing.city)) {
                     return res.status(400).send({ status: false, message: "billing city is required" })
                 }
-                if (!/^[a-zA-Z]+$/.test(billing.city)) {
+                if (!/^[a-zA-Z]+$/g.test(billing.city)) {
                     return res.status(400).send({ status: false, message: "city field have to fill by alpha characters" });
                 }
     
@@ -194,7 +193,7 @@ const isValidRequestBody = function (value) {
                 }
 
                  //applicable only for numeric values and extend to be 6 characters only--
-                if (!/^\d{6}$/.test(billing.pincode)) {
+                if (!/^\d{6}$/g.test(billing.pincode)) {
                     return res.status(400).send({ status: false, message: "plz enter valid  billing pincode" });
                 }
         
@@ -215,6 +214,63 @@ const isValidRequestBody = function (value) {
         
     }
      catch (error) {
+        return res.status(500).send({ status: "error", message: error.message })      
+    }
+}
+
+
+
+const logIn = async(req,res)=>{
+    try{
+      let data = req.body
+      let {email, password} = data
+
+      if(!email || email.length == 0){
+          return res.status(400).send({status : false, message : "Email must be provide"})
+      }
+
+      if(email){
+          let emailPattern = /^\w+([\.-]?\w+)@\w+([\.-]?\w+)(\.\w{2,3})/g 
+          if(!email.match(emailPattern)){
+              return res.status(400).send({status : false, message : 'please provide a valid email'})
+          }
+      }
+      
+      if(!password || password.length == 0){
+        return res.status(400).send({status : false, message : "password must be provide"})
+      }
+
+      if (password.trim().length < 8 || password.trim().length > 15) {
+        return res.status(400).send({ status: false, message: "plzz enter valid password" })
+     }
+
+     let emailExt = await user.findOne({email : email})
+
+     if(!emailExt){
+        return res.status(404).send({ status: false, message: "an account with this email does not exists" })
+     }
+
+     let comparePassword = await bcrypt.compare( this.password, emailExt.password)
+
+     if(!comparePassword){
+       return res.status(400).send({status : false, message : "Please provide a valid password"})
+     }else{
+         let params = {
+             userId : emailExt._id,
+             iat : Date.now()
+         }
+
+         let secretKey = 'vjfjdaehvkxfpekfpekfojdsopfjsdaoifji'
+
+        let token = jwt.sign(params, secretKey)
+
+        res.header('x-api-key', token)
+
+        return res.status(200).send({status : true, message : "User logIn Successfull", data : {userId : emailExt._id, token : token }})
+     }
+
+    }
+    catch (error) {
         return res.status(500).send({ status: "error", message: error.message })      
     }
 }
