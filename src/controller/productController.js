6d8e796e1d92f8c1e20a0fc6747ce2ac36e3afab
itstheanurag <gauravanurag36@gmail.com>
2022-05-25 16:15:46 +0530

    So tired

--- src/controller/productController.js
+++ src/controller/productController.js
@@ -165,5 +165,156 @@ const getProductByID = async (req,res)=>{
 }
 
 
+const updateProduct  = async (req, res)=>{
+    try{
 
-module.exports = {createProduct, getProductByID}
+        let data = req.body
+
+        if(!isValidRequestBody(data)){
+            return res.status(400).send({status : false, message : "No input has been provided"})
+        }
+
+        let ProductId = req.params.productId
+
+        if(!ProductId){
+            return res.status(400).send({status : false, message :"Product id is required to this action"})
+        }
+
+        if(!isValid(ProductId)){
+            return res.status(400).send({status : false, message : "Product id is missing"})
+        }
+
+        if(!mongoose.isValidObjectId(ProductId)){
+            return res.status(400).send({status : false, message : "Please Provide a valid Product Id"})
+        }
+
+        let findProduct = await product.findOne({_id : ProductId})
+
+        if(!findProduct){
+            return res.status(404).send({status : false, message : 'No product with this id exists'})
+        }
+
+        if(findProduct.isDeleted){
+            return res.status(404).send({status :false, message : 'This product is deleted'})
+        }
+
+        let {title, description, price, currencyId, currencyFormat, isFreeShipping, style, availableSize, installment} = data
+
+        if(title){
+        
+            if(!isValid(title)){
+                return res.status(400).send({status : false, message : "This is not a valid title"})
+            }
+
+            let dupTitle = await product.findOne({title : title})
+
+            if(dupTitle){
+                return res.status(409).send({status : false, message : "This title is already being used"})
+            }
+        }
+
+        if(description){ 
+            if(!isValid(description)){
+                return res.status(400).send({status : false, message : "This is not a valid description"})
+            }
+        }
+
+        if(price){
+            if(!isValid(price)){
+                return res.status(400).send({status : false, message : "please enter a price"})
+            }
+        }
+
+        if(currencyId){
+            if(!isValid(currencyId)){
+                return res.status(400).send({status : false, message : "please enter a currency id"})
+            }
+
+            if(currencyId !== "INR"){
+                return res.status(400).send({status : false, message : "Only INR is accepted as Currency INR"})
+            }
+        }
+
+        if(currencyFormat){
+            if(!isValid(currencyFormat)){
+                return res.status(400).send({status : false, message : "please enter a currency format"})
+            }
+
+            if(currencyId !== "₹"){
+                return res.status(400).send({status : false, message : "currency format must be ₹ "})
+            }
+        }
+
+        if(availableSize){
+        
+            if(!isValid(availableSize)){
+                return res.status(400).send({status : false, message : "please provide valid input"})
+            }
+            let sizeArray = ["S", "XS","M","X", "L","XXL", "XL"]
+
+            if(!sizeArray.includes(availableSize)){
+                return res.status(400).send({status : false, message : `Available sizes must be in ${sizeArray}` })
+            }
+        }
+
+        if(installment){
+            if(!isValid(installment)){
+                return res.status(400).send({status : false, message : "Please provide valid installment"})
+            }
+        }
+
+        let files = req.files
+
+        if(files && files.length>0){
+            let uploadedFileURL = await uploadFile(files[0]);
+            data["productImage"] = uploadedFileURL;
+        }
+
+        let updateProduct= await product.findOneAndUpdate({_id :ProductId}, {$set : {...data}, updatedAt: Date.now()}, {new : true, upsert : true})
+        
+        return res.status(200).send({status : true,  message : "Updated Successfully", data : updateProduct})
+
+    }catch (error) {
+        return res.status(500).send({ status: false, message: error.message })      
+    }
+}
+
+const deleteProduct = async (req, res)=>{
+    try{
+        let id = req.params.productId
+
+        if(!id){
+            return res.status(400).send({status : false, message : "product Id must be provided in order to delete it"})
+        }
+
+        if(!isValid(id)){
+            return res.status(400).send({status : false, message : "product id is missing"})
+        }
+
+        if(!mongoose.isValidObjectId(id)){
+            return res.status(400).send({status : false, message : "please provide a valid user id"})
+        }
+
+        let findProduct = await product.findById({_id : id})
+
+        if(!findProduct){
+            return res.status(404).send({status : false, message : "No product with this id exists"})
+        }
+
+        if(findProduct.isDeleted){
+            return res.status(404).send({status :false, message : 'This product is deleted'})
+        }
+
+        let deleteProduct = await product.findOneAndUpdate({_id : id}, {$set : {isDeleted : true}, deletedAt : Date.now()}, {new : true, upsert: true})
+
+        return res.status(200).send({status : true, message : "Product deleted successfully", data : deleteProduct})
+
+    }catch (error) {
+        return res.status(500).send({ status: false, message: error.message })      
+    }
+}
+
+
+
+
+module.exports = {createProduct, getProductByID, updateProduct, deleteProduct}
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const { createProduct, getProductByID, updateProduct, deleteProduct } = require('../controller/productController')
 const { createUser, logIn, findProfile, updateProfile } = require('../controller/userControllers')
 const { authentication } = require('../middleware/auth')
 const router = express.Router()
@@ -9,5 +10,14 @@ router.post('/register', createUser)
 router.post('/login', logIn)
 router.get('/user/:userId/profile', authentication, findProfile)
 router.put('/user/:userId/profile', authentication, updateProfile)
+
+//feature 2
+
+router.post('/products', createProduct)
+router.get('/products')
+router.get('/products/:productId', getProductByID)
+router.put('/products/:productId', updateProduct)
+router.delete('/products/:productId', deleteProduct)
+
 
 module.exports = router;
