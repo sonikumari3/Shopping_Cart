894d9099821b05ccc1b949413e6381cce368278f
itstheanurag <gauravanurag36@gmail.com>
2022-05-30 12:55:09 +0530

    Done

--- src/controller/orderControllers.js
+++ src/controller/orderControllers.js
@@ -2,7 +2,7 @@ const { default: mongoose } = require("mongoose")
 const order = require("../model/orderModel")
 const user = require("../model/userModel")
 const cart = require("../model/cartModel")
-const { isValid, isValidRequestBody } = require("../validations/validations")
+const { isValid, isValidRequestBody, isValidStatus } = require("../validations/validations")
 
 
 const createOrder = async (req, res)=>{
@@ -33,7 +33,7 @@ const createOrder = async (req, res)=>{
            return res.status(400).send({status : false, message : "No input has been provided"})
        }
 
-       let cartId = data.cartId
+       let {cartId, status, cancellable} = data
        
        if(!cartId){
            return res.status(400).send({status : false, message : "Cart Id is a required field"})
@@ -47,24 +47,47 @@ const createOrder = async (req, res)=>{
            return res.status(400).send({status : false, message : "please provide a valid cartId"})
        }
 
+       if(status){
+           if(!isValidStatus(status)){
+               return res.status(400).send({status : false, message : " status can only be, 'pending', 'completed' or 'canceled"})
+           }
+       }
+
+       if(cancellable){
+           if(["true", "false"].includes(cancellable) === false){
+               return res.status(400).send({status : false, message : "cancellable only take a boolean value"})
+           }
+       }
+
        let findCart = await cart.findById({_id : cartId})
 
        if(!findCart){
            return res.status(404).send({status : false, message : " No cart with this cart id exists"})
        }
 
+       if(findCart.userId != userId){
+           return res.status(401).send({status : false, message : "This cart does not belong to you"})
+       }
+
+       let totalQuantity = 0;
+       for(let i in findCart.items){
+           totalQuantity += findCart.items[i].quantity
+        }
+
        let orderData = {
            userId : userId,
            items : findCart.items,
            totalPrice : findCart.totalPrice,
            totalItems : findCart.totalItems,
-           totalQuantity : findCart.items.quantity
+           totalQuantity : totalQuantity,
+           status : status,
+           cancellable : cancellable
        }
 
        let createOrder = await order.create(orderData)
        
        return res.status(201).send({status : true, message : "order created successfully", data : createOrder})
-       
+
    } catch (error) {
         return res.status(500).send({ status: "error", message: error.message });
    }
